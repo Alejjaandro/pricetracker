1febaa414440d8ea3f9d2ead180a8c16cda94e74
Alejjaandro <alejandroolaso98@gmail.com>
2023-11-21 12:12:05 +0100

    Searchbar URL check

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,63 @@
 "use client"
 
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
+
+const isValidAmazonURL = (url: string) => {
+    try {
+        const parsedURL = new URL(url);
+        const hostname = parsedURL.hostname;
+
+        if(
+            hostname.includes('amazon.com') || 
+            hostname.includes('amazon.') ||
+            hostname.includes('amazon')
+        ) {
+            return true
+        }
+    } catch (error) {
+        return false
+    }
+}
 
 const Searchbar = () => {
 
-    const handleSubmit = () => { }
+    const [searchPromp, setSearchPromp] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => { 
+        event.preventDefault();
+
+        const isValidLink = isValidAmazonURL(searchPromp);
+
+        if(!isValidLink) return alert('Please enter a valid Amazon Product URL')
+
+        try {
+            setIsLoading(true);
+
+            // Scrape the product web
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
         <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
-            <input type="text" placeholder='Enter product Link' className='searchbar-input' />
 
-            <button type='submit' className='searchbar-btn'>Search</button>
+            <input
+                type="text"
+                value={searchPromp}
+                onChange={(e) => setSearchPromp(e.target.value)}
+                placeholder='Enter product Link'
+                className='searchbar-input'
+            />
+
+            <button type='submit' className='searchbar-btn' disabled={searchPromp === ''}>
+                {isLoading ? 'Searching...' : 'Search'}
+            </button>
+
         </form >
     )
 }
