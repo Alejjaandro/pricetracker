8a8abfaa27bcd6b3b7d671f4df0b9f2775c49b1b
Alejjaandro <alejandroolaso98@gmail.com>
2023-11-21 11:48:58 +0100

    ProductCarousel and Searchbar components

---
+++ components/ProductCarousel.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+
+const productsImages = [
+    { src: '/assets/images/hero-1.svg', alt: 'smartwatch' },
+    { src: '/assets/images/hero-2.svg', alt: 'bag' },
+    { src: '/assets/images/hero-3.svg', alt: 'lamp' },
+    { src: '/assets/images/hero-4.svg', alt: 'air fryer' },
+    { src: '/assets/images/hero-5.svg', alt: 'chair' }
+]
+
+const ProductCarousel = () => {
+    return (
+        <div className='product-carousel'>
+            <Carousel
+                showThumbs={false}
+                autoPlay
+                infiniteLoop
+                interval={2000}
+                showArrows={false}
+                showStatus={false}
+            >
+                {productsImages.map(
+                    (product) => (
+                        <Image src={product.src} alt={product.alt} width={484} height={484}
+                            key={product.alt}
+                            className='object-contain'
+                        />
+                    )
+                )}
+            </Carousel>
+
+            <Image src='/assets/icons/hand-drawn-arrow.svg' alt='arrow' width={175} height={175}
+                className='max-xl:hidden absolute -left-[15%] bottom-0'
+            />
+        </div>
+    )
+}
+
+export default ProductCarousel
